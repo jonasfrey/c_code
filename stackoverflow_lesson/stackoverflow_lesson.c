aa54ee050d7dd0ff5bc0aa6feeeff65d4982406c
ubuntuuser <jonas.immanuel.frey@gmail.com>
2022-01-30 19:05:38 +0100

    stackoverflow

--- stackoverflow_lesson/stackoverflow_lesson.c
+++ stackoverflow_lesson/stackoverflow_lesson.c
@@ -2,6 +2,7 @@
 // c file 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 void is_this_heap_overflow(){
    long n = 1000;
@@ -43,13 +44,172 @@ void heapoverflow3(){
 
 }
 
+long long stack_overflow_recursion(long long i){
+   i = i+1; 
+   if(i < 100000){
+   // if(i < 1000000){ // segmentation fault
+   // if(i < 10000000){ // segmentation fault
+
+      return stack_overflow_recursion(i); // some really bad recursion here
+   }
+   return i;
+}
+
+void stack_overflow(){
+   long long i = 10; 
+   i = stack_overflow_recursion(i);
+   printf("%lli", i);
+   fflush(stdout);
+}
+
 void trying_to_cause_stackoverflow(){
 
 }
 
+int create_c_file_with_many_vars(){
+
+
+   printf("----------------------------------------\n");
+   printf("test starting\n");
+   FILE * file_pointer;
+
+   // use appropriate location if you are using MacOS or Linux
+   long j = 1000000;
+   printf("creating file with %li variables\n", j);
+
+   char file_name[150];
+   char file_path_name[200];
+   
+   sprintf(file_name,"c_file_with_many_vars_%li",j);
+   sprintf(file_path_name,"./%s.c",file_name);
+
+
+   remove(file_path_name);
+   file_pointer = fopen(file_path_name, "a");// a=append
+   if(file_pointer == NULL){
+      printf("fopen error");
+      exit(1);
+   }
+
+   fprintf(file_pointer,"//file is generated automatically by c function fopen()\n");
+   fprintf(file_pointer,"#include <stdio.h>\n");
+   fprintf(file_pointer,"int main(){\n");
+   long i = 0;
+
+   char var_name[20]; 
+   for(i = 0; i < j; i++){
+      sprintf(var_name, "variable_number_%li", i);
+      fprintf(file_pointer, "long long %s = %li;\n", var_name,i);
+   }
+   
+   fprintf(file_pointer, "printf(\"last variable name:value, %s:%li\");\n", var_name, i);
+
+   fprintf(file_pointer,"}\n");
+   fclose(file_pointer);
+
+
+   printf("compiling ...\n");
+   char compile_command[500]; 
+   sprintf(compile_command, "gcc %s -o %s", file_path_name, file_name);
+   system(compile_command);
+
+   printf("running binary... \n");
+   char run_binary_command[500]; 
+   sprintf(run_binary_command, "./%s", file_name);
+   system(run_binary_command);
+
+   printf("----------------------------------------\n");
+   printf("test finished\n");
+
+   return 0;
+
+}
+
+
+void stack_overflow2(){
+
+   char a_really_huge_char_which_should_cause_stackoverflow[1000000000]; 
+   printf("%s", a_really_huge_char_which_should_cause_stackoverflow); 
+
+}
+
+
+void stack_overflow_int_array(){
+//run this programm to check at what size stackoverflow happens for your pc 
+//mine happens at 
+//   array size:              2100000 -> 2'100'000 at 2 million 
+//                               so the actual bytes are 2 million * sizeof(int) which is 4 on this machine
+//                            2100000 * 4 = 8'400'000 bytes
+//                            8'400'000 / 1024 = 8203 kilobytes
+//                                8'203 / 1024 =    8 megabytes
+
+   printf("sizeof(short): %li\n", sizeof(int));
+   sleep(2);
+   for(int i = 0; i<(10000000); i++){
+      if(i%10000 == 0){
+         printf("   array size: %20i\n", i);
+         int huge_int_array[i];
+         huge_int_array[0] = 11235; 
+         printf("huge_int_array[0]: %20i\n", huge_int_array[0]);
+      }
+   }
+
+   // int huge_2_d_array[1000][10000];
+   // huge_2_d_array[0][0] = 1;
+   // printf("huge_2_d_array[0][0]: %i\n", huge_2_d_array[0][0]);
+
+}
+void stack_overflow_shortint_array(){
+// segmentation fault happens at size
+//array size:              4190000 -> 4'190'000 at 4 million
+   printf("sizeof(short): %li\n", sizeof(short));
+   sleep(2);
+
+   for(int i = 0; i<(1000000000); i++){
+      if(i%10000 == 0){
+         printf("   array size: %20i\n", i);
+         short huge_shortint_array[i];
+         huge_shortint_array[0] = 11235; 
+         printf("huge_shortint_array[0]: %20i\n", huge_shortint_array[0]);
+      }
+   }
+
+}
+void stack_overflow_longint_array(){
+//run this programm to check at what size stackoverflow happens for your pc 
+//mine happens at 
+//   array size:              1050000 -> 1'050'000 at 1 million
+   printf("sizeof(long): %li\n", sizeof(int));
+   sleep(2);
+   for(int i = 0; i<(10000000); i++){
+      if(i%10000 == 0){
+         printf("   array size: %20i\n", i);
+         long huge_longint_array[i];
+         huge_longint_array[0] = 11235; 
+         printf("huge_longint_array[0]: %20li\n", huge_longint_array[0]);
+      }
+   }
+
+   // int huge_2_d_array[1000][10000];
+   // huge_2_d_array[0][0] = 1;
+   // printf("huge_2_d_array[0][0]: %i\n", huge_2_d_array[0][0]);
+
+}
 int main() {
+   
+   // WARNING, its highly recommended to try this stuff with a Virtual Machine!
+   // the following c programms CAN CRASH your PC! 
+
    // is_this_heap_overflow();   
    // heapoverflow2();
-   heapoverflow3();
+   // heapoverflow3();
+   // stack_overflow();
+   // create_c_file_with_many_vars();
+   // stack_overflow2();
+   stack_overflow_int_array();
+   // stack_overflow_shortint_array();
+   // stack_overflow_longint_array();
+
    return 0;
+
 }
